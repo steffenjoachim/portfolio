45aadcc2afa2f0b8891d8fd40f2c855e5cbeba44
steffenjoachim <steffenjoachim.schanze@gmail.com>
2023-09-19 17:10:21 +0200

    final version

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -12,7 +12,7 @@ export class PortfolioComponent {
 = [
     { "name": "Join", "skills": "JavaScript | HTML | CSS", "description": "Task manager inspired by Kanban System, create and organize tasks using drag and drop functions, assign users and categories.", "serverUrl": "https://steffen-schanze.de/Join", "gitUrl": "https://github.com/steffenjoachim/KANBAN-Board", "imgUrl": "/assets/img/join.png", "order": "odd" },
     { "name": "El Pollo Loco", "skills": "JavaScript | HTML | CSS", "description": "A simple Jump-and-Run game based on object-oriented approach. Help Pepe in the desert to fight chickens and collect bottels to finally concer the end-boss.", "serverUrl": "https://steffen-schanze.de/El-Pollo-loco", "gitUrl": "https://github.com/steffenjoachim/El-Pollo-loco", "imgUrl": "/assets/img/El_Pollo_Loco.png", "order": "even" },
-    { "name": "Yet to Come", "skills": "Angular | TypeScript | SCSS", "description": "Task manager inspired by Kanban System, create and organize tasks using drag and drop functions, assign users and categories.", "serverUrl": "https://steffen-schanze.developerakademie.net/KANBAN-Board", "gitUrl": "https://github.com/steffenjoachim/KANBAN-Board", "imgUrl": "/assets/img/join.png", "order": "odd" },
+    { "name": "My simple CRM", "skills": "Angular | TypeScript | Firebase", "description": "Very simple CRM app to demonstrate the four basic CRUD functions based on an Angular/Firebase project.", "serverUrl": "https://steffen-schanze.de/my-simple-crm", "gitUrl": "https://github.com/steffenjoachim/my-simple-crm", "imgUrl": "/assets/img/my-simple-crm.png", "order": "odd" },
   ];
 
 }
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import * as Aos from 'aos';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  Aos.init(
+    {
+      offset: 300,
+      duration: 800,
+    }
+  );
